727c98d83d63d9a7c5ffcfb1d86ff38772417ed3
Jere Niemi <jere.viljami.niemi@gmail.com>
2023-11-19 22:33:20 +0200

    Testi nodeja

---
+++ ViewModels/IntegerValueEditorViewModel.cs
@@ -0,0 +1,19 @@
+﻿using NodeNetworkTesti.Views;
+using NodeNetwork.Toolkit.ValueNode;
+using ReactiveUI;
+
+namespace NodeNetworkTesti.ViewModels
+{
+    public class IntegerValueEditorViewModel : ValueEditorViewModel<int?>
+    {
+        static IntegerValueEditorViewModel()
+        {
+            Splat.Locator.CurrentMutable.Register(() => new IntegerValueEditorView(), typeof(IViewFor<IntegerValueEditorViewModel>));
+        }
+
+        public IntegerValueEditorViewModel()
+        {
+            Value = 0;
+        }
+    }
+}
